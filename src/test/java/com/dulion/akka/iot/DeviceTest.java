e2736fe58942ac77b36f8d95faee0ded468116eb
PHILLIP DULION <phillip@dulion.com>
2024-07-06 14:14:23 -0700

    Cleanup

--- src/test/java/com/dulion/akka/iot/DeviceTest.java
+++ src/test/java/com/dulion/akka/iot/DeviceTest.java
@@ -1,14 +1,11 @@
 package com.dulion.akka.iot;
 
 import akka.actor.testkit.typed.javadsl.TestKitJunitResource;
-import akka.actor.testkit.typed.javadsl.TestProbe;
-import akka.actor.typed.ActorRef;
 import com.dulion.akka.iot.Device.Passivate;
 import com.dulion.akka.iot.Device.ReadTemperatureReply;
 import com.dulion.akka.iot.Device.ReadTemperatureRequest;
 import com.dulion.akka.iot.Device.RecordTemperatureReply;
 import com.dulion.akka.iot.Device.RecordTemperatureRequest;
-import com.dulion.akka.iot.Device.Request;
 import com.dulion.akka.iot.Manager.AllTemperaturesReply;
 import com.dulion.akka.iot.Manager.AllTemperaturesRequest;
 import com.dulion.akka.iot.Manager.DeviceListReply;
@@ -34,53 +31,51 @@ public class DeviceTest {
 
   @Test
   public void testReplyWithEmptyReadingIfNoTemperatureIsKnown() {
-    TestProbe<ReadTemperatureReply> readProbe = testKit.createTestProbe(ReadTemperatureReply.class);
-    ActorRef<Request> deviceActor = testKit.spawn(Device.create("group", "device"));
+    var readProbe = testKit.createTestProbe(ReadTemperatureReply.class);
+    var deviceActor = testKit.spawn(Device.create("group", "device"));
     deviceActor.tell(ReadTemperatureRequest.of(42L, readProbe.getRef()));
-    ReadTemperatureReply reply = readProbe.receiveMessage();
+    var reply = readProbe.receiveMessage();
     assertEquals(42L, reply.getRequestId());
     assertNull(reply.getTemperature());
   }
 
   @Test
   public void testReplyWithLatestTemperatureReading() {
-    TestProbe<RecordTemperatureReply> recordProbe = testKit.createTestProbe(RecordTemperatureReply.class);
-    TestProbe<ReadTemperatureReply> readProbe = testKit.createTestProbe(ReadTemperatureReply.class);
-    ActorRef<Request> deviceActor = testKit.spawn(Device.create("group", "device"));
+    var recordProbe = testKit.createTestProbe(RecordTemperatureReply.class);
+    var readProbe = testKit.createTestProbe(ReadTemperatureReply.class);
+    var deviceActor = testKit.spawn(Device.create("group", "device"));
 
     deviceActor.tell(RecordTemperatureRequest.of(1L, 24.0, recordProbe.getRef()));
     assertEquals(1L, recordProbe.receiveMessage().getRequestId());
 
     deviceActor.tell(ReadTemperatureRequest.of(2L, readProbe.getRef()));
-    ReadTemperatureReply temperature = readProbe.receiveMessage();
+    var temperature = readProbe.receiveMessage();
     assertEquals(2L, temperature.getRequestId());
     assertEquals(24.0, temperature.getTemperature(), 0.0);
   }
 
   @Test
   public void testReplyToRegistrationRequests() {
-    TestProbe<RegisterDeviceReply> registeredProbe = testKit.createTestProbe(RegisterDeviceReply.class);
-    ActorRef<Group.Request> groupActor = testKit.spawn(Group.create("group"));
+    var registeredProbe = testKit.createTestProbe(RegisterDeviceReply.class);
+    var groupActor = testKit.spawn(Group.create("group"));
 
     groupActor.tell(RegisterDeviceRequest.of("group", "device1", registeredProbe.getRef()));
-    RegisterDeviceReply registered1 = registeredProbe.receiveMessage();
-
+    var registered1 = registeredProbe.receiveMessage();
     groupActor.tell(RegisterDeviceRequest.of("group","device2", registeredProbe.getRef()));
-    RegisterDeviceReply registered2 = registeredProbe.receiveMessage();
+    var registered2 = registeredProbe.receiveMessage();
     assertNotEquals(registered1.getDevice(), registered2.getDevice());
 
-    TestProbe<ReadTemperatureReply> readProbe = testKit.createTestProbe(ReadTemperatureReply.class);
+    var readProbe = testKit.createTestProbe(ReadTemperatureReply.class);
     registered1.getDevice().tell(ReadTemperatureRequest.of(1L, readProbe.getRef()));
     assertEquals(1L, readProbe.receiveMessage().getRequestId());
-
     registered2.getDevice().tell(ReadTemperatureRequest.of(2L, readProbe.getRef()));
     assertEquals(2L, readProbe.receiveMessage().getRequestId());
   }
 
   @Test
   public void testIgnoreWrongRegistrationRequests() {
-    TestProbe<RegisterDeviceReply> registeredProbe = testKit.createTestProbe(RegisterDeviceReply.class);
-    ActorRef<Group.Request> groupActor = testKit.spawn(Group.create("group"));
+    var registeredProbe = testKit.createTestProbe(RegisterDeviceReply.class);
+    var groupActor = testKit.spawn(Group.create("group"));
 
     groupActor.tell(RegisterDeviceRequest.of("wrong","device", registeredProbe.getRef()));
     registeredProbe.expectNoMessage();
@@ -88,33 +83,33 @@ public class DeviceTest {
 
   @Test
   public void testReturnSameActorForSameDeviceId() {
-    TestProbe<RegisterDeviceReply> registeredProbe = testKit.createTestProbe(RegisterDeviceReply.class);
-    ActorRef<Group.Request> groupActor = testKit.spawn(Group.create("group"));
+    var registeredProbe = testKit.createTestProbe(RegisterDeviceReply.class);
+    var groupActor = testKit.spawn(Group.create("group"));
 
     groupActor.tell(RegisterDeviceRequest.of("group", "device", registeredProbe.getRef()));
-    RegisterDeviceReply registered1 = registeredProbe.receiveMessage();
+    var registered1 = registeredProbe.receiveMessage();
 
     groupActor.tell(RegisterDeviceRequest.of("group","device", registeredProbe.getRef()));
-    RegisterDeviceReply registered2 = registeredProbe.receiveMessage();
+    var registered2 = registeredProbe.receiveMessage();
 
     assertEquals(registered1.getDevice(), registered2.getDevice());
   }
 
   @Test
   public void testListActiveDevices() {
-    TestProbe<RegisterDeviceReply> registeredProbe = testKit.createTestProbe(RegisterDeviceReply.class);
-    ActorRef<Group.Request> groupActor = testKit.spawn(Group.create("group"));
+    var registeredProbe = testKit.createTestProbe(RegisterDeviceReply.class);
+    var groupActor = testKit.spawn(Group.create("group"));
 
     groupActor.tell(RegisterDeviceRequest.of("group", "device1", registeredProbe.getRef()));
-    RegisterDeviceReply registered1 = registeredProbe.receiveMessage();
+    var registered1 = registeredProbe.receiveMessage();
 
     groupActor.tell(RegisterDeviceRequest.of("group","device2", registeredProbe.getRef()));
-    RegisterDeviceReply registered2 = registeredProbe.receiveMessage();
+    var registered2 = registeredProbe.receiveMessage();
     assertNotEquals(registered1.getDevice(), registered2.getDevice());
 
-    TestProbe<DeviceListReply> deviceListProbe = testKit.createTestProbe(DeviceListReply.class);
+    var deviceListProbe = testKit.createTestProbe(DeviceListReply.class);
     groupActor.tell(DeviceListRequest.of(1L, "group", deviceListProbe.getRef()));
-    DeviceListReply deviceList = deviceListProbe.receiveMessage();
+    var deviceList = deviceListProbe.receiveMessage();
     assertEquals(1L, deviceList.getRequestId());
     assertEquals(
         Stream.of("device1", "device2").collect(Collectors.toSet()),
@@ -123,19 +118,19 @@ public class DeviceTest {
 
   @Test
   public void testListActiveDevicesAfterOneShutsDown() {
-    TestProbe<RegisterDeviceReply> registeredProbe = testKit.createTestProbe(RegisterDeviceReply.class);
-    ActorRef<Group.Request> groupActor = testKit.spawn(Group.create("group"));
+    var registeredProbe = testKit.createTestProbe(RegisterDeviceReply.class);
+    var groupActor = testKit.spawn(Group.create("group"));
 
     groupActor.tell(RegisterDeviceRequest.of("group", "device1", registeredProbe.getRef()));
-    RegisterDeviceReply registered1 = registeredProbe.receiveMessage();
+    var registered1 = registeredProbe.receiveMessage();
 
     groupActor.tell(RegisterDeviceRequest.of("group","device2", registeredProbe.getRef()));
-    RegisterDeviceReply registered2 = registeredProbe.receiveMessage();
+    var registered2 = registeredProbe.receiveMessage();
     assertNotEquals(registered1.getDevice(), registered2.getDevice());
 
-    TestProbe<DeviceListReply> deviceListProbe = testKit.createTestProbe(DeviceListReply.class);
+    var deviceListProbe = testKit.createTestProbe(DeviceListReply.class);
     groupActor.tell(DeviceListRequest.of(1L, "group", deviceListProbe.getRef()));
-    DeviceListReply deviceList1 = deviceListProbe.receiveMessage();
+    var deviceList1 = deviceListProbe.receiveMessage();
     assertEquals(1L, deviceList1.getRequestId());
     assertEquals(
         Stream.of("device1", "device2").collect(Collectors.toSet()),
@@ -169,7 +164,7 @@ public class DeviceTest {
     groupActor.tell(RegisterDeviceRequest.of("group","device2", registeredProbe.getRef()));
     var device2 = registeredProbe.receiveMessage();
     groupActor.tell(RegisterDeviceRequest.of("group","device3", registeredProbe.getRef()));
-    var device3 = registeredProbe.receiveMessage();
+    registeredProbe.receiveMessage();
 
     // Add temperature to devices
     var recordProbe = testKit.createTestProbe(Device.RecordTemperatureReply.class);
